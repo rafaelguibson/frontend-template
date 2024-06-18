fa04d253ef77a0a3c9d912f8f33ecb25e3332e0b
Rafael Guibson <rafaelguibsons@gmail.com>
2024-06-18 09:59:31 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {CustomSidenavComponent} from "./components/custom-sidenav/custom-sidenav.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, CustomSidenavComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
 
+    <mat-toolbar class="mat-elevation-z4">
+      <button mat-icon-button (click)="collapsed.set(!collapsed())">
+        <mat-icon>menu</mat-icon>
+      </button>
+    </mat-toolbar>
+    <mat-sidenav-container>
+      <mat-sidenav opened mode="side" [style.width]="sidenavWidth()">
+        <app-custom-sidenav></app-custom-sidenav>
+      </mat-sidenav>
+      <mat-sidenav-content class="content" [style.margin-left]="sidenavWidth()">
+        <h2>Hello</h2>
+            <router-outlet></router-outlet>
+      </mat-sidenav-content>
+    </mat-sidenav-container>
     <router-outlet />
+
+
   `,
-  styles: [],
+  styles: [`
+    mat-toolbar {
+      position: relative;
+      z-index: 5;
+    }
+    .content {
+    padding: 24px;
+    }
+    mat-sidenav-container {
+      height: calc(100vh - 64px);
+    }
+  `],
 })
 export class AppComponent {
-  title = 'prototype';
+
+  collapsed = signal(false);
+
+  sidenavWidth = computed(() => this.collapsed() ? '65px' : '250px')
 }
---
+++ src/app/components/custom-sidenav/custom-sidenav.component.ts
@@ -0,0 +1,81 @@
+import {Component, signal} from '@angular/core';
+import {NgForOf, NgOptimizedImage} from "@angular/common";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+
+export type MenuItem = {
+  icon: string;
+  label: string;
+  route:string;
+}
+
+@Component({
+  selector: 'app-custom-sidenav',
+  standalone: true,
+  imports: [
+    NgOptimizedImage, MatListModule, MatIconModule, NgForOf
+  ],
+  template: `
+    <div class="sidenav-header">
+      <img width="100" height="100" src="assets/profile.jpg" alt="profile-image" border="3px solid #000"/>
+      <div class="header-text">
+        <h2>Meu Delírio</h2>
+        <p>Bem-Vindo!</p>
+      </div>
+    </div>
+    <mat-nav-list>
+      <a mat-list-item *ngFor="let item of menuItems()">
+        <mat-icon matListItemIcon>{{item.icon}}</mat-icon>
+        <span matListItemTitle>{{item.label}}</span>
+      </a>
+    </mat-nav-list>
+  `,
+  styles: [`
+  .sidenav-header {
+    padding-top: 24px;
+    text-align: center;
+
+    > img {
+      border-radius: 100%;
+      object-fit: cover;
+      margin-bottom: 10px;
+    }
+    .header-text {
+      > h2 {
+        margin: 0;
+        font-size: 1rem;
+        line-height: 1.5rem;
+      }
+      > p {
+        margin: 0;
+        font-size: 0.8rem;
+      }
+    }
+  }
+  `]
+})
+export class CustomSidenavComponent {
+
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard'
+    },
+    {
+      icon: 'video_library',
+      label: 'Content',
+      route: 'content'
+    },
+    {
+      icon: 'analytics',
+      label: 'Analytis',
+      route: 'analytics'
+    },
+    {
+      icon: 'comment',
+      label: 'Comment',
+      route: 'comments'
+    },
+  ]);
+}
